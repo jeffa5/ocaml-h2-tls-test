8064d0c6e4c9111db69bb06600b165c06614dfc2
Andrew Jeffery <dev@jeffas.io>
2020-12-04 18:52:48 +0000

    Update to not need patches to gluten or h2

--- bin.ml
+++ bin.ml
@@ -9,6 +9,12 @@ let () =
      in
      let socket = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
      let* () = Lwt_unix.connect socket (List.hd addresses).Unix.ai_addr in
+     let null_auth ~host:_ _ = Ok None in
+     let config =
+       Tls.Config.client ~authenticator:null_auth ~alpn_protocols:[ "h2" ]
+         ~ciphers:Tls.Config.Ciphers.http2 ()
+     in
+     let* tls_client = Tls_lwt.Unix.client_of_fd config socket in
      let error_handler = function
        | `Malformed_response s -> print_endline ("malformed response: " ^ s)
        | `Invalid_response_body_length _res ->
@@ -19,7 +25,6 @@ let () =
        | `Exn exn -> print_endline (Printexc.to_string exn)
      in
      let+ _connection =
-       H2_lwt_unix.Client.TLS.create_connection_with_default ~error_handler
-         socket
+       H2_lwt_unix.Client.TLS.create_connection ~error_handler tls_client
      in
      ())
